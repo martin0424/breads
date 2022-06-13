fcd845a64b7d9d462656cca51a4b80928397e88f
martin0424 <h.newland424@gmail.com>
2022-06-13 19:17:13 -0400

    updated package.json and breads controllers

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,11 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+// SHOW
+breads.get('/arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+})
+
 // INDEX
 breads.get('/', (req,res) => {
     res.send(Bread)
