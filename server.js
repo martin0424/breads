bbcf6111f0ed6e13ef15874b4995490e1c021410
martin0424 <h.newland424@gmail.com>
2022-06-13 20:07:18 -0400

    updated server.js with express-react-views

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require('dotenv').config();
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
@@ -14,6 +19,7 @@ app.get('/', (req, res) => {
 // Breads
 const breadController = require('./controllers/breads_controller')
 app.use('/breads', breadController)
+
 // LISTEN
 app.listen(PORT, () => {
     console.log('nomming at port', PORT);
